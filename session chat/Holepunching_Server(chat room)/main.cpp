ef8b52ecaa2b234c94c4416b9ba0f4c4941ae327
PPODO <sherlockwhooo@gmail.com>
2020-03-07 00:35:13 +0900

    Update main.cpp

--- session chat/Holepunching_Server(chat room)/main.cpp
+++ session chat/Holepunching_Server(chat room)/main.cpp
@@ -13,12 +13,169 @@
 
 constexpr size_t BUFFER_SIZE = 1024;
 
-enum EMESSAGETYPE : uint16_t {
-	EMT_JOIN,
-	EMT_FIND,
-	EMT_CREATE,
-	EMT_DISCONNECT
-};
+#pragma once
+#include <iostream>
+#include <sstream>
+#include <vector>
+
+namespace Packets {
+	namespace MessageType {
+		enum EMESSAGETYPE : uint16_t {
+			EMT_CONNECT = 1,
+			EMT_JOIN,
+			EMT_FIND,
+			EMT_CREATE,
+			EMT_DISCONNECT
+		};
+	}
+
+	namespace Types {
+		struct CSessionInfo {
+		public:
+			std::string m_SessionName;
+			uint16_t m_SessionID;
+			uint16_t m_MaximumCount;
+			uint16_t m_CurrentCount;
+			bool m_bUsePassword;
+
+		public:
+			friend std::ostream& operator<<(std::ostream& os, const CSessionInfo& SessionInfo) {
+				size_t Length = SessionInfo.m_SessionName.length();
+				os.write(reinterpret_cast<const char*>(&Length), sizeof(size_t));
+				os.write(SessionInfo.m_SessionName.c_str(), SessionInfo.m_SessionName.length());
+				os.write(reinterpret_cast<const char*>(&SessionInfo.m_SessionID), sizeof(SessionInfo.m_SessionID));
+				os.write(reinterpret_cast<const char*>(&SessionInfo.m_MaximumCount), sizeof(SessionInfo.m_MaximumCount));
+				os.write(reinterpret_cast<const char*>(&SessionInfo.m_CurrentCount), sizeof(SessionInfo.m_CurrentCount));
+				os.write(reinterpret_cast<const char*>(&SessionInfo.m_bUsePassword), sizeof(SessionInfo.m_bUsePassword));
+
+				return os;
+			}
+
+			friend std::istream& operator>>(std::istream& is, CSessionInfo& SessionInfo) {
+				size_t Length;
+				is.read(reinterpret_cast<char*>(&Length), sizeof(size_t));
+				char* TempBuffer = new char[Length];
+				is.read(TempBuffer, Length);
+				SessionInfo.m_SessionName = std::string(TempBuffer);
+				delete[] TempBuffer;
+
+				is.read(reinterpret_cast<char*>(&SessionInfo.m_SessionID), sizeof(SessionInfo.m_SessionID));
+				is.read(reinterpret_cast<char*>(&SessionInfo.m_MaximumCount), sizeof(SessionInfo.m_MaximumCount));
+				is.read(reinterpret_cast<char*>(&SessionInfo.m_CurrentCount), sizeof(SessionInfo.m_CurrentCount));
+				is.read(reinterpret_cast<char*>(&SessionInfo.m_bUsePassword), sizeof(SessionInfo.m_bUsePassword));
+
+				return is;
+			}
+
+		};
+
+		struct CPacket {
+		public:
+			unsigned short m_MessageType;
+
+		public:
+			CPacket(const unsigned short _MessageType) : m_MessageType(_MessageType) {};
+
+		public:
+			friend std::ostream& operator<<(std::ostream& os, const CPacket& Packet) {
+				os.write(reinterpret_cast<const char*>(&Packet.m_MessageType), sizeof(Packet.m_MessageType));
+
+				return os;
+			}
+
+			friend std::istream& operator>>(std::istream& is, CPacket& Packet) {
+				is.read(reinterpret_cast<char*>(&Packet.m_MessageType), sizeof(Packet.m_MessageType));
+
+				return is;
+			}
+
+		};
+
+		struct CJoinPacket : public CPacket {
+		public:
+			uint16_t m_SessionID;
+
+		public:
+			CJoinPacket(uint16_t SessionID) : CPacket(MessageType::EMESSAGETYPE::EMT_JOIN), m_SessionID(SessionID) {};
+
+		public:
+			friend std::ostream& operator<<(std::ostream& os, const CJoinPacket& Packet) {
+				os.write(reinterpret_cast<const char*>(&Packet.m_MessageType), sizeof(Packet.m_MessageType));
+				os.write(reinterpret_cast<const char*>(&Packet.m_SessionID), sizeof(Packet.m_SessionID));
+
+				return os;
+			}
+
+			friend std::istream& operator>>(std::istream& is, CJoinPacket& Packet) {
+				is.read(reinterpret_cast<char*>(&Packet.m_MessageType), sizeof(Packet.m_MessageType));
+				is.read(reinterpret_cast<char*>(&Packet.m_SessionID), sizeof(Packet.m_SessionID));
+
+				return is;
+			}
+
+		};
+
+		struct CFindPacket : public CPacket {
+		public:
+			std::vector<CSessionInfo> m_SessionInformation;
+
+		public:
+			CFindPacket() : CPacket(MessageType::EMESSAGETYPE::EMT_FIND) {};
+
+		public:
+			friend std::ostream& operator<<(std::ostream& os, const CFindPacket& Packet) {
+				os.write(reinterpret_cast<const char*>(&Packet.m_MessageType), sizeof(Packet.m_MessageType));
+				size_t Size = Packet.m_SessionInformation.size();
+				os.write(reinterpret_cast<const char*>(&Size), sizeof(size_t));
+				for (const auto& It : Packet.m_SessionInformation) {
+					os << It;
+				}
+
+				return os;
+			}
+
+			friend std::istream& operator>>(std::istream& is, CFindPacket& Packet) {
+				is.read(reinterpret_cast<char*>(&Packet.m_MessageType), sizeof(Packet.m_MessageType));
+				size_t Size;
+				is.read(reinterpret_cast<char*>(&Size), sizeof(Size));
+				for (int i = 0; i < Size; i++) {
+					CSessionInfo Info;
+					is >> Info;
+					Packet.m_SessionInformation.insert(Packet.m_SessionInformation.begin(), 0, Info);
+				}
+
+				return is;
+			}
+
+		};
+
+
+		struct CCreatePacket : public CPacket {
+		public:
+			CSessionInfo m_SessionInformation;
+
+		public:
+			CCreatePacket(const CSessionInfo& SessionInfo) : CPacket(MessageType::EMESSAGETYPE::EMT_CREATE), m_SessionInformation(SessionInfo) {};
+
+		public:
+			friend std::ostream& operator<<(std::ostream& os, const CCreatePacket& Packet) {
+				os.write(reinterpret_cast<const char*>(&Packet.m_MessageType), sizeof(Packet.m_MessageType));
+				os << Packet.m_SessionInformation;
+
+				return os;
+			}
+
+			friend std::istream& operator>>(std::istream& is, CCreatePacket& Packet) {
+				is.read(reinterpret_cast<char*>(&Packet.m_MessageType), sizeof(Packet.m_MessageType));
+				is >> Packet.m_SessionInformation;
+
+				return is;
+			}
+
+		};
+
+	}
+}
 
 class MessageQueue {
 private:
@@ -50,74 +207,6 @@ public:
 	}
 };
 
-void JoinClient(SOCKET ListenSocket, std::stringstream& is, const sockaddr_in& RecvAddress, std::vector<std::map<unsigned short, sockaddr_in>>& SessionList, std::mutex& SessionMutex) {
-	uint16_t SessionID;
-	is >> SessionID;
-
-	try {
-		std::lock_guard<std::mutex> Lock(SessionMutex);
-		
-		auto Key = (RecvAddress.sin_addr.S_un.S_addr + RecvAddress.sin_port) % 100;
-		if (auto Iterator = SessionList[SessionID].find(Key); Iterator == SessionList[SessionID].cend()) {
-			{
-				std::stringstream os;
-				os << EMT_JOIN << std::endl << 1 << std::endl << RecvAddress.sin_addr.S_un.S_addr << std::endl << RecvAddress.sin_port << std::endl;
-
-				for (auto Client : SessionList[SessionID]) {
-					sendto(ListenSocket, os.str().c_str(), os.str().length(), 0, reinterpret_cast<sockaddr*>(&Client.second), sizeof(sockaddr_in));
-				}
-			}
-
-			{
-				std::stringstream os;
-				os << EMT_JOIN << std::endl << SessionList[SessionID].size() << std::endl;
-
-				for (auto Client : SessionList[SessionID]) {
-					os << Client.second.sin_addr.S_un.S_addr << std::endl << Client.second.sin_port << std::endl;
-				}
-				sendto(ListenSocket, os.str().c_str(), os.str().length(), 0, reinterpret_cast<const sockaddr*>(&RecvAddress), sizeof(sockaddr_in));
-			}
-		}
-		else {
-			std::cout << "existing client!\n";
-		}
-	}
-	catch (const std::out_of_range& exception) {
-		std::cout << exception.what() << std::endl;
-	}
-}
-
-void FindSession(std::stringstream& is) {
-
-}
-
-void CreateSession(std::stringstream& is) {
-
-}
-
-void DisconnectClient(SOCKET ListenSocket, std::stringstream& is, const sockaddr_in& RecvAddress, std::vector<std::map<unsigned short, sockaddr_in>>& SessionList, std::mutex& SessionMutex) {
-	uint16_t SessionID;
-	is >> SessionID;
-
-	try {
-		std::lock_guard<std::mutex> Lock(SessionMutex);
-
-		auto Key = ((RecvAddress.sin_addr.S_un.S_addr + RecvAddress.sin_port) % 100);
-		if (auto Iterator = SessionList[SessionID].find(Key); Iterator != SessionList[SessionID].cend()) {
-			std::stringstream os;
-			os << EMESSAGETYPE::EMT_DISCONNECT << std::endl << Key << std::endl;
-
-			SessionList[SessionID].erase(Iterator);
-			for (auto Client : SessionList[SessionID]) {
-				sendto(ListenSocket, os.str().c_str(), os.str().length(), 0, reinterpret_cast<sockaddr*>(&Client.second), sizeof(sockaddr_in));
-			}
-		}
-	}
-	catch (const std::out_of_range& exception) {
-		std::cout << exception.what() << std::endl;
-	}
-}
-
 int main() {
 	WSADATA WinSockData;
 	if (WSAStartup(WINSOCK_VERSION, &WinSockData) != 0) {
@@ -140,50 +229,15 @@ int main() {
 		return -1;
 	}
 
-	MessageQueue Queue;
 	int AddrLength = sizeof(sockaddr_in);
-	std::mutex SessionMutex;
-	std::vector<std::map<unsigned short, sockaddr_in>> Sessions;
-	std::atomic_bool RunState(true);
-	std::thread WorkerThread([&ListenSocket, &RunState, &Queue, &Sessions, &SessionMutex]() {
-		while (RunState) {
-			auto Result = Queue.pop();
-			std::stringstream is(std::get<0>(Result));
-			uint16_t MessageType;
-			is >> MessageType;
-
-			switch (MessageType) {
-			case EMESSAGETYPE::EMT_JOIN:
-				JoinClient(is);
-				break;
-			case EMESSAGETYPE::EMT_FIND:
-				FindSession(is);
-				break;
-			case EMESSAGETYPE::EMT_CREATE:
-				CreateSession(is);
-				break;
-			case EMESSAGETYPE::EMT_DISCONNECT:
-				DisconnectClient(ListenSocket, is, std::get<1>(Result), Sessions, SessionMutex);
-				break;
-			default:
-				std::cout << "unknown message type!\n";
-				break;
-			}
-		}
-		});
-	
 	while (true) {
 		sockaddr_in RecvAddress;
-		if (char MessageBuffer[BUFFER_SIZE] = { "\0" }; recvfrom(ListenSocket, MessageBuffer, BUFFER_SIZE, 0, reinterpret_cast<sockaddr*>(&RecvAddress), &AddrLength)) {
-			Queue.push(MessageBuffer);
+		if (char MessageBuffer[BUFFER_SIZE] = { "\0" }; recvfrom(ListenSocket, MessageBuffer, BUFFER_SIZE, 0, reinterpret_cast<sockaddr*>(&RecvAddress), &AddrLength) > 0) {
 			std::cout << "Recv From : " << inet_ntoa(RecvAddress.sin_addr) << '\t' << RecvAddress.sin_port << std::endl;
+			sendto(ListenSocket, MessageBuffer, strlen(MessageBuffer), 0, reinterpret_cast<sockaddr*>(&RecvAddress), sizeof(sockaddr_in));
 		}
 	}
 
-	if (WorkerThread.joinable()) {
-		RunState.exchange(false);
-		WorkerThread.join();
-	}
 	closesocket(ListenSocket);
 	WSACleanup();
 	return 0;
