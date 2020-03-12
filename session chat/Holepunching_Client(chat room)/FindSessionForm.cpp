2006eab1f0ccd4993b3447fbfbc356b91e4ef34e
PPODO <sherlockwhooo@gmail.com>
2020-03-12 21:52:37 +0900

    Update FindSessionForm

--- session chat/Holepunching_Client(chat room)/FindSessionForm.cpp
+++ session chat/Holepunching_Client(chat room)/FindSessionForm.cpp
@@ -4,11 +4,10 @@
 namespace HolepunchingClientchatroom {
 	System::Void FindSessionForm::FindSessionForm_Load(System::Object^ sender, System::EventArgs^ e) {
 		if (this->Owner->Validate()) {
-			this->Owner->Enabled = false;
-			
 			std::stringstream Stream;
 			auto Owner = dynamic_cast<MainForm^>(this->Owner);
 			Owner->FindDelegate = gcnew FindSessionPacketDelegate(this, &FindSessionForm::PacketProcessing);
+			Owner->JoinDelegate += gcnew JoinSessionPacketDelegate(this, &FindSessionForm::JoinSession_Delegate);
 			Stream << Packets::Types::CFindPacket();
 			AsyncSocket::SendTo(Owner->SocketObject, gcnew String(Stream.str().c_str(), 0, Stream.str().length()));
 		}
@@ -16,11 +15,36 @@ namespace HolepunchingClientchatroom {
 
 	System::Void FindSessionForm::FindSessionForm_Closed(System::Object^ sender, System::EventArgs^ e) {
 		if (this->Owner->Validate()) {
-			this->Owner->Enabled = true;
+			auto Owner = dynamic_cast<MainForm^>(this->Owner);
+			Owner->FindDelegate -= gcnew FindSessionPacketDelegate(this, &FindSessionForm::PacketProcessing);
+			Owner->JoinDelegate -= gcnew JoinSessionPacketDelegate(this, &FindSessionForm::JoinSession_Delegate);
 		}
 	}
 
-	System::Void FindSessionForm::PacketProcessing(const Packets::Types::CFindPacket& Packet) {
+	System::Void FindSessionForm::JoinButton_Click(System::Object^ sender, System::EventArgs^ e) {
+		if (auto FocusedItem = SessionList->FocusedItem) {
+			auto Item = this->SessionList->Items[FocusedItem->Index];
 
+			std::stringstream Stream;
+			Stream << Packets::Types::CJoinPacket(Packets::Types::CSessionInfo(Convert::ToInt16(Item->Text)));
+			AsyncSocket::SendTo(dynamic_cast<MainForm^>(this->Owner)->SocketObject, gcnew String(Stream.str().c_str(), 0, Stream.str().length()));
+		}
+	}
+
+	System::Void FindSessionForm::PacketProcessing(array<unsigned char>^ Buffer) {
+		pin_ptr<unsigned char> Ptr = &Buffer[0];
+		Packets::Types::CFindPacket Packet{ std::stringstream(std::string(reinterpret_cast<char*>(Ptr), Buffer->Length)) };
+		
+		for (auto Session : Packet.m_SessionInformation) {
+			System::Windows::Forms::ListViewItem^ Item = gcnew System::Windows::Forms::ListViewItem(gcnew array<String^>(5) {
+				Session.m_SessionID.ToString(), gcnew String(Session.m_SessionName.c_str()), Session.m_CurrentCount.ToString(),
+				Session.m_MaximumCount.ToString(), Session.m_bUsePassword.ToString()
+			});
+			this->SessionList->Items->Add(Item);
+		}
+	}
+
+	System::Void FindSessionForm::JoinSession_Delegate(array<unsigned char>^ Buffer) {
+		this->Close();
 	}
 }
--- session chat/Holepunching_Client(chat room)/FindSessionForm.h
+++ session chat/Holepunching_Client(chat room)/FindSessionForm.h
@@ -30,19 +30,25 @@ namespace HolepunchingClientchatroom {
 
 	private:
 		System::Windows::Forms::ListView^ SessionList;
+		System::Windows::Forms::ColumnHeader^ IDHeader;
 		System::Windows::Forms::ColumnHeader^ NameHeader;
 		System::Windows::Forms::ColumnHeader^ CurrentHeader;
 		System::Windows::Forms::ColumnHeader^ MaxHeader;
 		System::Windows::Forms::ColumnHeader^ UsePasswordHeader;
 
 	private:
+		System::Windows::Forms::Button^ JoinButton;
+
+	private:
 
 		void InitializeComponent(System::Windows::Forms::Form^ Owner) {
 			SessionList = gcnew System::Windows::Forms::ListView();
+			IDHeader = gcnew System::Windows::Forms::ColumnHeader();
 			NameHeader = gcnew System::Windows::Forms::ColumnHeader();
 			CurrentHeader = gcnew System::Windows::Forms::ColumnHeader();
 			MaxHeader = gcnew System::Windows::Forms::ColumnHeader();
 			UsePasswordHeader = gcnew System::Windows::Forms::ColumnHeader();
+			JoinButton = gcnew System::Windows::Forms::Button();
 
 			this->Owner = Owner;
 			this->SuspendLayout();
@@ -59,16 +65,20 @@ namespace HolepunchingClientchatroom {
 			this->ResumeLayout(false);
 
 			{
+				this->IDHeader->Text = L"Session ID";
 				this->NameHeader->Text = L"Name";
 				this->CurrentHeader->Text = L"Current";
 				this->MaxHeader->Text = L"Max";
 				this->UsePasswordHeader->Text = L"Use Password";
 
-				this->NameHeader->Width = 550 / 4;
-				this->CurrentHeader->Width = 550 / 4;
-				this->MaxHeader->Width = 550 / 4;
-				this->UsePasswordHeader->Width = 550 / 4;
+				const int Width = 550 / 5;
+				this->IDHeader->Width = Width;
+				this->NameHeader->Width = Width;
+				this->CurrentHeader->Width = Width;
+				this->MaxHeader->Width = Width;
+				this->UsePasswordHeader->Width = Width;
 
+				this->IDHeader->TextAlign = System::Windows::Forms::HorizontalAlignment::Center;
 				this->CurrentHeader->TextAlign = System::Windows::Forms::HorizontalAlignment::Center;
 				this->MaxHeader->TextAlign = System::Windows::Forms::HorizontalAlignment::Center;
 				this->UsePasswordHeader->TextAlign = System::Windows::Forms::HorizontalAlignment::Center;
@@ -82,19 +92,36 @@ namespace HolepunchingClientchatroom {
 				this->SessionList->UseCompatibleStateImageBehavior = false;
 				this->SessionList->TabIndex = 0;
 				this->SessionList->View = System::Windows::Forms::View::Details;
-				this->SessionList->Columns->AddRange(gcnew array<System::Windows::Forms::ColumnHeader^>(4) {
-					this->NameHeader, this->CurrentHeader, this->MaxHeader, this->UsePasswordHeader
+				this->SessionList->Columns->AddRange(gcnew array<System::Windows::Forms::ColumnHeader^>(5) {
+					this->IDHeader, this->NameHeader, this->CurrentHeader, this->MaxHeader, this->UsePasswordHeader
 				});
 			}
 
+			{
+				this->JoinButton->BackColor = System::Drawing::SystemColors::ButtonHighlight;
+				this->JoinButton->Font = gcnew System::Drawing::Font(L"NanumSquare ExtraBold", 16, System::Drawing::FontStyle::Bold, System::Drawing::GraphicsUnit::Point, 129);
+				this->JoinButton->Size = System::Drawing::Size(200, 50);
+				this->JoinButton->Location = System::Drawing::Point((this->Size.Width / 2) - 100, this->SessionList->Size.Height + this->SessionList->Location.Y + 15);
+				this->JoinButton->TabIndex = 2;
+				this->JoinButton->Name = L"Join Button";
+				this->JoinButton->Text = L"Join";
+				this->JoinButton->UseVisualStyleBackColor = false;
+				this->JoinButton->Click += gcnew System::EventHandler(this, &FindSessionForm::JoinButton_Click);
+			}
+
 			this->Controls->Add(this->SessionList);
+			this->Controls->Add(this->JoinButton);
 		}
 	private:
 		System::Void FindSessionForm_Load(System::Object^ sender, System::EventArgs^ e);
 		System::Void FindSessionForm_Closed(System::Object^ sender, System::EventArgs^ e);
 
 	private:
-		System::Void PacketProcessing(const Packets::Types::CFindPacket& Packet);
+		System::Void JoinButton_Click(System::Object^ sender, System::EventArgs^ e);
+
+	private:
+		System::Void PacketProcessing(array<unsigned char>^ Buffer);
+		System::Void JoinSession_Delegate(array<unsigned char>^ Buffer);
 
 	};
 }
