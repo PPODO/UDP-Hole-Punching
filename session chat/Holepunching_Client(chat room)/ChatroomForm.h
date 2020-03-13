72e003c46b17d9d8c0c66b99dcde30669a6865a5
PPODO <sherlockwhooo@gmail.com>
2020-03-14 00:52:20 +0900

    Update ChatroomForm.h

--- session chat/Holepunching_Client(chat room)/ChatroomForm.h
+++ session chat/Holepunching_Client(chat room)/ChatroomForm.h
@@ -26,9 +26,44 @@ namespace HolepunchingClientchatroom {
 		System::ComponentModel::Container^ components;
 
 	private:
-		void InitializeComponent(String^ SessionName, System::Windows::Forms::Form^ Owner) {
-			this->Owner = Owner;
+		System::Windows::Forms::RichTextBox^ ChattingTextBox;
+		System::Windows::Forms::RichTextBox^ ChatInputTextBox;
+		System::Windows::Forms::ListView^ UserListView;
 
+	private:
+		void InitializeComponent(String^ SessionName, System::Windows::Forms::Form^ Owner) {
+			this->ChattingTextBox = gcnew System::Windows::Forms::RichTextBox();
+			this->ChatInputTextBox = gcnew System::Windows::Forms::RichTextBox();
+			this->UserListView = gcnew System::Windows::Forms::ListView();
+
+			{
+				this->ChattingTextBox->Name = "Chatting TextBox";
+				this->ChattingTextBox->Size = System::Drawing::Size(1000, 75);
+				this->ChattingTextBox->Location = System::Drawing::Point((1080 / 2) - 500, 500);
+				this->ChattingTextBox->TabIndex = 3;
+			}
+
+			{
+				this->ChatInputTextBox->BackColor = System::Drawing::SystemColors::ActiveBorder;
+				this->ChatInputTextBox->Name = "Chat Input TextBox";
+				this->ChatInputTextBox->Size = System::Drawing::Size(850, 475);
+				this->ChatInputTextBox->Location = System::Drawing::Point(40, 15);
+				this->ChatInputTextBox->ReadOnly = true;
+				this->ChatInputTextBox->TabIndex = 2;
+			}
+
+			{
+				this->UserListView->HideSelection = false;
+				this->UserListView->Name = L"User ListView";
+				this->UserListView->Size = System::Drawing::Size(125, 475);
+				this->UserListView->Location = System::Drawing::Point(915, 15);
+				this->UserListView->UseCompatibleStateImageBehavior = false;
+				this->UserListView->TabIndex = 0;
+				this->UserListView->View = System::Windows::Forms::View::Details;
+				this->UserListView->Columns->Add(L"Name", 125, System::Windows::Forms::HorizontalAlignment::Center);
+			}
+
+			this->Owner = Owner;
 			this->SuspendLayout();
 			this->AutoScaleDimensions = System::Drawing::SizeF(7, 12);
 			this->AutoScaleMode = System::Windows::Forms::AutoScaleMode::Font;
@@ -38,6 +73,10 @@ namespace HolepunchingClientchatroom {
 			this->Load += gcnew System::EventHandler(this, &ChatroomForm::ChatroomForm_Load);
 			this->Closed += gcnew System::EventHandler(this, &ChatroomForm::ChatroomForm_Closed);
 			this->ResumeLayout(false);
+
+			this->Controls->Add(ChattingTextBox);
+			this->Controls->Add(ChatInputTextBox);
+			this->Controls->Add(UserListView);
 		}
 
 	private: 
